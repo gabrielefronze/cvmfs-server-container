329db3f528fa9e219642bdb1c90aa083fe5e5934
Gabriele <sucre.91@hotmail.it>
2019-04-09 16:30:51 +0200

    Using frontier proxy for client conenction

--- cvmfs-client/client-init.sh
+++ cvmfs-client/client-init.sh
@@ -1,7 +1,7 @@
 # Setup of connection configuration
 echo "CVMFS_SERVER_URL=$CVMFS_STRATUM1_URL/cvmfs/$CVMFS_REPO_NAME" > /etc/cvmfs/default.local
 echo "CVMFS_REPOSITORIES=$CVMFS_REPO_NAME" >> /etc/cvmfs/default.local
-echo "CVMFS_HTTP_PROXY=DIRECT" >> /etc/cvmfs/default.local
+echo "CVMFS_HTTP_PROXY=http://virgo-test-02.to.infn.it:3128" >> /etc/cvmfs/default.local
 echo "CVMFS_CACHE_BASE=/cvmfs-cache" >> /etc/cvmfs/default.local
 echo "CVMFS_QUOTA_LIMIT=10240" >> /etc/cvmfs/default.local
 

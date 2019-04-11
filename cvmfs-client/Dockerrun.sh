e315c85422ddae27cd2d7ef6351951441cfa3e8e
Gabriele <sucre.91@hotmail.it>
2019-04-11 16:53:24 +0200

    Fixing client Dockerrun script

--- cvmfs-client/Dockerrun.sh
+++ cvmfs-client/Dockerrun.sh
@@ -1,8 +1,6 @@
-docker build -t slidspitfire/cvmfs-client-latest:latest .
-
 docker run -t \
 --name cvmfs-client \
 --hostname cvmfs-client \
 --privileged \
 --volume /sys/fs/cgroup:/sys/fs/cgroup \
-slidspitfire/cvmfs-client-latest:latest
+slidspitfire/cvmfs-client

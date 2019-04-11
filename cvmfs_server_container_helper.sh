72a3d5c29e126c13ca32f1e7a5adde7f8835f024
Gabriele <sucre.91@hotmail.it>
2019-04-11 16:38:36 +0200

    Removing additional dash from container base name

--- cvmfs_server_container_helper.sh
+++ cvmfs_server_container_helper.sh
@@ -9,7 +9,7 @@
 
 export CVMFS_SERVER_GIT_URL=https://github.com/gabrielefronze/cvmfs-server-container.git
 export CVMFS_SERVER_LOCAL_GIT_REPO=~/cvmfs-server-container
-export CVMFS_CONTAINER_BASE_IMAGE_NAME=slidspitfire/cvmfs-
+export CVMFS_CONTAINER_BASE_IMAGE_NAME=slidspitfire/cvmfs
 export CVMFS_STRATUM_CONTAINER="dummy"
 export DEFAULT_HOST_CVMFS_ROOT_DIR=/scratch/cvmfs-docker
 export CVMFS_LOG_DIR=/var/log/cvmfs-server-container

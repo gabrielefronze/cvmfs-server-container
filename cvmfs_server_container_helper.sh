38f95cf62413454018def69bae93dae2af9a29a0
gabrielefronze <sucre.91@hotmail.it>
2019-05-14 17:49:35 +0200

    Prompting for stratum selection whenever both found

--- cvmfs_server_container_helper.sh
+++ cvmfs_server_container_helper.sh
@@ -19,32 +19,30 @@ if [[ ! -d "$CVMFS_LOG_DIR" ]]; then
 fi
 
 function prompt_stratum_selection {
-    if [[ "$CVMFS_STRATUM_CONTAINER"=="dummy" ]]; then
-        if [[ $(docker ps | grep -c "cvmfs-stratum0") == 1 ]]; then
-            if [[ $(docker ps | grep -c "cvmfs-stratum1") == 1 ]]; then
-                read -p "Both stratum0 and stratum1 are running. Please enter [0/1]: " strindex
+    if [[ $(docker ps | grep -c "cvmfs-stratum0") == 1 ]]; then
+        if [[ $(docker ps | grep -c "cvmfs-stratum1") == 1 ]]; then
+            read -p "Both stratum0 and stratum1 are running. Please enter [0/1]: " strindex
 
-                case "$strindex" in
-                0)
-                    CVMFS_STRATUM_CONTAINER="cvmfs-stratum0"
-                    ;;
-                1)
-                    CVMFS_STRATUM_CONTAINER="cvmfs-stratum1"
-                    ;;
-                *)
-                    echo "FATAL: Unsupported option. Please use [0/1]"
-                    exit 1
-                    ;;
-                esac
-            else
+            case "$strindex" in
+            0)
                 CVMFS_STRATUM_CONTAINER="cvmfs-stratum0"
-            fi
-        elif [[ $(docker ps | grep -c "cvmfs-stratum1") == 1 ]]; then
-            CVMFS_STRATUM_CONTAINER="cvmfs-stratum1"
+                ;;
+            1)
+                CVMFS_STRATUM_CONTAINER="cvmfs-stratum1"
+                ;;
+            *)
+                echo "FATAL: Unsupported option. Please use [0/1]"
+                exit 1
+                ;;
+            esac
         else
-            echo "FATAL: No cvmfs container found. Please run it or manually set the CVMFS_STRATUM_CONTAINER environment variable."
-            exit 1
+            CVMFS_STRATUM_CONTAINER="cvmfs-stratum0"
         fi
+    elif [[ $(docker ps | grep -c "cvmfs-stratum1") == 1 ]]; then
+        CVMFS_STRATUM_CONTAINER="cvmfs-stratum1"
+    else
+        echo "FATAL: No cvmfs container found. Please run it or manually set the CVMFS_STRATUM_CONTAINER environment variable."
+        exit 1
     fi
 }
 

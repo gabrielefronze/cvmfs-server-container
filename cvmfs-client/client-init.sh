a97cae7dbdd43a43a57f0e576227e4fc34e6ff51
gabrielefronze <sucre.91@hotmail.it>
2019-05-16 17:44:33 +0200

    Fixing Sara's suggestions :)

--- cvmfs-client/client-init.sh
+++ cvmfs-client/client-init.sh
@@ -9,7 +9,7 @@ SERVER_NAME="${2:-cvmfs-stratum1}"
 echo "Setting up client for repo $REPO_NAME on server $SERVER_NAME."
 
 # Setup of connection configuration
-echo "CVMFS_SERVER_URL=http://$SERVER_NAME" > /etc/cvmfs/default.local
+echo "CVMFS_SERVER_URL=http://$SERVER_NAME/cmvfs/$REPO_NAME" > /etc/cvmfs/default.local
 echo "CVMFS_REPOSITORIES=$REPO_NAME" >> /etc/cvmfs/default.local
 echo "CVMFS_HTTP_PROXY=http://$SERVER_NAME:3128" >> /etc/cvmfs/default.local
 echo "CVMFS_CACHE_BASE=/cvmfs-cache" >> /etc/cvmfs/default.local

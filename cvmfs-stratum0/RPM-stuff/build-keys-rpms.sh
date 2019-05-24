f74cb9aa84d62ffa1899f92076e36a71227957bd
gabrielefronze <sucre.91@hotmail.it>
2019-05-24 15:11:53 +0200

    Adding automatic rpms generation script

---
+++ cvmfs-stratum0/RPM-stuff/build-keys-rpms.sh
@@ -0,0 +1,34 @@
+RPM_STUFF_PATH="~/RPM-stuff"
+
+CVMFS_REPO_NAME="$1"
+
+source setup-rpmspecs.sh "$CVMFS_REPO_NAME"
+
+OPTION="$2"
+        
+case "$OPTION" in
+    "--pub")
+        echo -n "Exporting public key for repo $CVMFS_REPO_NAME... "
+        source "$RPM_STUFF_PATH"/setup-rpmspecs.sh "$CVMFS_REPO_NAME"
+        rpmbuild -ba "$RPM_STUFF_PATH"/cvmfs-pub-key-"$CVMFS_REPO_NAME".spec
+        rm -f "$RPM_STUFF_PATH"/*"$CVMFS_REPO_NAME".spec
+        cp -f ~/rpmbuild/RPMS/x86_64/cvmfs-"$CVMFS_REPO_NAME"-pub-key-1-1.x86_64.rpm /etc/cvmfs/keys
+        echo "done"
+        echo "The public key is available at /etc/cvmfs/keys/cvmfs-$CVMFS_REPO_NAME-pub-key-1-1.x86_64.rpm"
+        ;;
+    "--relman")
+        echo -n "Exporting release manager keys set for $CVMFS_REPO_NAME... "
+        source "$RPM_STUFF_PATH"/setup-rpmspecs.sh "$CVMFS_REPO_NAME"
+        rpmbuild -ba "$RPM_STUFF_PATH"/cvmfs-relman-key-"$CVMFS_REPO_NAME".spec
+        rm -f "$RPM_STUFF_PATH"/*"$CVMFS_REPO_NAME".spec
+        cp -f ~/rpmbuild/RPMS/x86_64/cvmfs-"$CVMFS_REPO_NAME"-relman-key-1-1.x86_64.rpm /etc/cvmfs/keys
+        echo "done"
+        echo "The public key is available at /etc/cvmfs/keys/cvmfs-$CVMFS_REPO_NAME-relman-key-1-1.x86_64.rpm"
+        ;;
+    *) 
+        echo "FATAL: the second argument should be either --pub (public key export) or --relman (to export all the keys needed by Release Managers. Aborting."
+        return
+        ;;
+esac
+
+unset RPM_STUFF_PATH

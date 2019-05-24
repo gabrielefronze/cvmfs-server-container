1c549ddc95ac9b0e03b413cd30680cb7475af1bc
gabrielefronze <sucre.91@hotmail.it>
2019-05-24 15:25:07 +0200

    Fixing get-keys-rpm

--- cvmfs_server_container_helper.sh
+++ cvmfs_server_container_helper.sh
@@ -267,8 +267,6 @@ function cvmfs_server_container {
     get-keys-rpm)
         prompt_stratum_selection
 
-        echo "PIPPO"
-
         CVMFS_REPO_NAME="${@: -1}"
 
         REPO_LIST=($(docker exec -ti "$CVMFS_STRATUM_CONTAINER" cvmfs_server list | awk '{print $1}'))
@@ -276,10 +274,9 @@ function cvmfs_server_container {
         IS_REPO_VALID=$(containsElement "$CVMFS_REPO_NAME" "${REPO_LIST[@]}")
 
         if [[ "$IS_REPO_VALID" == "found" ]]; then
-            docker exec -ti "$CVMFS_STRATUM_CONTAINER" cvmfs_server "$@"
+            #do nothing
         elif [[ "$REPO_LIST_LENGTH" == 1 ]]; then
             CVMFS_REPO_NAME="${REPO_LIST[0]}"
-            docker exec -ti "$CVMFS_STRATUM_CONTAINER" cvmfs_server "$@"
         else
             echo "FATAL: multiple repos available, but no one specified as argument. Aborting."
             unset REPO_LIST

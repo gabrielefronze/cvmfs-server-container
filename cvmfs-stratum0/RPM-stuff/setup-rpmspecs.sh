81413c622b227f8ebb83c4804adc6d68ece0318e
gabrielefronze <sucre.91@hotmail.it>
2019-05-24 15:11:30 +0200

    Enforing overwrite

--- cvmfs-stratum0/RPM-stuff/setup-rpmspecs.sh
+++ cvmfs-stratum0/RPM-stuff/setup-rpmspecs.sh
@@ -8,8 +8,8 @@ PUB_KEY_RPM_NAME=cvmfs-pub-key-"$REPO_NAME".spec
 RELMAN_KEY_RPM_NAME=cvmfs-relman-key-"$REPO_NAME".spec
 
 cd "$HOME"/RPM-stuff
-cp cvmfs-pub-key-template.spec "$PUB_KEY_RPM_NAME"
-cp cvmfs-relman-key-template.spec "$RELMAN_KEY_RPM_NAME"
+cp -f cvmfs-pub-key-template.spec "$PUB_KEY_RPM_NAME"
+cp -f cvmfs-relman-key-template.spec "$RELMAN_KEY_RPM_NAME"
 sed -i "s/REPO_NAME_REPLACE_ME/${REPO_NAME}/g" "$PUB_KEY_RPM_NAME"
 sed -i "s/REPO_NAME_REPLACE_ME/${REPO_NAME}/g" "$RELMAN_KEY_RPM_NAME"
 cd -

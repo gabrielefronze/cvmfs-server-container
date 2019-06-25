4d73fbf0d8569b0c3620adc4d8d11309699ae46a
gabrielefronze <sucre.91@hotmail.it>
2019-06-25 12:21:21 +0200

    Logging timers operations

--- cvmfs-server-base/systemd-timers/cvmfs-on-all-repos.sh
+++ cvmfs-server-base/systemd-timers/cvmfs-on-all-repos.sh
@@ -1,3 +1,14 @@
+LOG_DIR="/var/log/cvmfs/"
+LOG_FILE="$LOG_DIR/cvmfs-timers.log"
+
+if [[ ! -d $LOG_DIR ]]; then
+    mkdir -p "$LOG_DIR"
+fi
+
+if [[ ! -f $LOG_FILE ]]; then
+    touch $LOG_FILE
+fi
+
 COMMAND="$1"
 
 if [[ "$COMMAND" == "resign" || "$COMMAND" == "snapshot" ]]; then
@@ -5,12 +16,12 @@ if [[ "$COMMAND" == "resign" || "$COMMAND" == "snapshot" ]]; then
 
     for REPO in $REPO_NAME_ARRAY; do
         TIMESTAMP=$(date -u)
-        echo -n "[$TIMESTAMP] performing cvmfs_server $COMMAND $REPO... "
-        cvmfs_server "$COMMAND" "$REPO"
+        echo -n "[$TIMESTAMP] performing cvmfs_server $COMMAND $REPO... " >> $LOG_FILE
+        cvmfs_server "$COMMAND" "$REPO" >> $LOG_FILE
     done
 else
     TIMESTAMP=$(date -u)
-    echo "[$TIMESTAMP] ERROR: This command is intended to be used just with resign or snapshot cvmfs commands."
+    echo "[$TIMESTAMP] ERROR: This command is intended to be used just with resign or snapshot cvmfs commands." >> $LOG_FILE
 fi
 
 unset COMMAND
